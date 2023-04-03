8f4041cf51f25ef469907d41dfc6d90c45e6fa08
Rohith <rohithsuresh207@gmail.com>
2023-04-03 11:58:54 +0530

    changing _axios base url for prod and new build

--- frontend/src/utils/_axios.ts
+++ frontend/src/utils/_axios.ts
@@ -1,4 +1,6 @@
 import Axios from "axios";
 
-const baseURL = "http://localhost:5000/api";
+const baseURL = import.meta.env.PROD
+	? "https://recurring-planner-monorepo.onrender.com/api"
+	: "http://localhost:5000/api";
 export const _axios = Axios.create({ baseURL, withCredentials: true });
