7706b4678132a4e9f5fce1552e1e4ce464bec9d0
Rohith <rohithsuresh207@gmail.com>
2023-04-03 11:57:00 +0530

    adding prod url to cors

--- utils/CORSMiddleware.go
+++ utils/CORSMiddleware.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		ctx.Writer.Header().Set("Access-Control-Allow-Origin", "https://recurring-planner-monorepo.onrender.com")
 		ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
