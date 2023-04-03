2378b63d430ba1bfb5a947c83059bf2a9e64d7e4
Rohith <rohithsuresh207@gmail.com>
2023-04-03 12:12:42 +0530

    fixing jwt token

--- middleware/auth.go
+++ middleware/auth.go
@@ -3,19 +3,24 @@ package middleware
 import (
 	"better_calendar_backend/utils"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func AuthMiddleware(c *gin.Context) {
 	authHeader := c.GetHeader("Authorization")
+	err := godotenv.Load()
+	utils.ErrorHandler(err)
+	jwtSecretString := os.Getenv("JWT_SECRET")
 	token := strings.Split(authHeader, " ")[1]
 	if authHeader != "" {
 		claims := jwt.MapClaims{}
 		user, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
-			return []byte("helloworld"), nil
+			return []byte(jwtSecretString), nil
 		})
 		utils.ErrorHandler(err)
 		c.Set("userId", int(user.Claims.(jwt.MapClaims)["userId"].(float64)))
